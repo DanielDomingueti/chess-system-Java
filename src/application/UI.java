bf78e5263016c58e053d3512f1f6c0252b297119
Daniel Domingueti <danieldomingueti@hotmail.com>
2021-05-11 20:06:43 -0300

    Board Exception (155)

--- src/application/UI.java
+++ src/application/UI.java
@@ -2,7 +2,7 @@ package application;
 
 import chess.ChessPiece;
 
-public class UI {
+public class UI {  //USER INTERFACE
 	
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i=0 ; i<pieces.length ; i++) {
